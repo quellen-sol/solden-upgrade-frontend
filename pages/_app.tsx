5ac437131953906f51e0b96dfb30a8e035792221
quellen-sol <marquelle.nesbitt@gmail.com>
2022-06-18 19:18:31 -0700

    Add fonts plus more setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import "antd/dist/antd.css";
+import "../styles/global.css";
 import type { AppProps } from "next/app";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  const RPC_URL = "https://mainnet-beta.solflare.network/";
+  const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
+
+  return (
+    <ConnectionProvider endpoint={RPC_URL}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 };
 
 export default MyApp;
